b607756cd2679e9f95a915502fd887b4fe2265e6
Jessica Yang <jessica.yang@berkeley.edu>
2020-06-09 22:14:23 -0700

    added explanations

--- interviewq/src/subsequence.java
+++ interviewq/src/subsequence.java
@@ -1,19 +1,16 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-// Given a string S and a set of words D, find the longest word in D that is a subsequence of S.
-//
-// Word W is a subsequence of S if some number of characters, possibly zero, can be deleted from S to form W, without reordering the remaining characters.
-//
-// Note: D can appear in any format (list, hash table, prefix tree, etc.
-//
-// For example, given the input of S = "abppplee" and D = {"able", "ale", "apple", "bale", "kangaroo"} the correct output would be "apple"
-//
-// The words "able" and "ale" are both subsequences of S, but they are shorter than "apple".
-// The word "bale" is not a subsequence of S because even though S has all the right letters, they are not in the right order.
-// The word "kangaroo" is the longest word in D, but it isn't a subsequence of S.
+/**
+ Question:
+ Given a string S and a set of words D, find the longest word in D that is a subsequence of S.
+ Word W is a subsequence of S if some number of characters, possibly zero, can be deleted from S to form W, without reordering the remaining characters.
+ Note: D can appear in any format (list, hash table, prefix tree, etc.
+ For example, given the input of S = "abppplee" and D = {"able", "ale", "apple", "bale", "kangaroo"} the correct output would be "apple"
+ The words "able" and "ale" are both subsequences of S, but they are shorter than "apple".
+ The word "bale" is not a subsequence of S because even though S has all the right letters, they are not in the right order.
+ The word "kangaroo" is the longest word in D, but it isn't a subsequence of S. **/
 
 public class subsequence {
     public static void main(String[] args) {
@@ -21,6 +18,12 @@ public class subsequence {
         System.out.println(find("abppplee", test));
     }
 
+    /** This was solved by doing one word at a time with a for loop.
+     * Each word is then checked by each letter, in order, to make sure the String  contains it.
+     * for example, the a in "apple" is checked to make sure it's containd in string "abppplee".
+     * Then, "p" is checked by a concated string "bppplee" (found by indexOf)
+     * Finally, if the entire word is found, (ie the for loop didn't break), it is added to a final list.
+     * The final list of all subsequences is sorted and the longest word is returned.*/
     public static String find(String s, List<String> d) {
         ArrayList<String> valids = new ArrayList<String>();
         for (String word : d) {
@@ -35,12 +38,11 @@ public class subsequence {
                     used = false;
                     break;
                 }
-
-            if (used) {
-                valids.add(word);
+                if (used) {
+                    valids.add(word);
+                }
             }
         }
-    }
         int max = valids.get(0).length();
         String longest = valids.get(0);
         for (String valid : valids) {
@@ -51,9 +53,6 @@ public class subsequence {
             }
 
         }
-
         return longest;
-
-}
-
+    }
 }
