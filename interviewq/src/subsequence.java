988eaa3dcb43ff6e79c263f5e7219144921d6dcf
Jessica Yang <jessica.yang@berkeley.edu>
2020-06-09 21:58:00 -0700

    solved

--- interviewq/src/subsequence.java
+++ interviewq/src/subsequence.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // Given a string S and a set of words D, find the longest word in D that is a subsequence of S.
@@ -16,15 +17,43 @@ import java.util.List;
 
 public class subsequence {
     public static void main(String[] args) {
-
+        List<String> test = Arrays.asList("able", "ale", "apple", "bale", "kangaroo");
+        System.out.println(find("abppplee", test));
     }
 
     public static String find(String s, List<String> d) {
-        ArrayList<String> valids = new ArrayList<>();
-        while (s.length() != 0) {
-            if (d.contains(s)) {
-                valids.add(s);
+        ArrayList<String> valids = new ArrayList<String>();
+        for (String word : d) {
+            String sCopy = s;
+            for (int i = 0; i < word.length(); i++) {
+                char current = word.charAt(i);
+                boolean used = true;
+                if (sCopy.contains(String.valueOf(current))) {
+                    int index = sCopy.indexOf(current);
+                    sCopy = sCopy.substring(index+1);
+                } else {
+                    used = false;
+                    break;
+                }
+
+            if (used) {
+                valids.add(word);
             }
         }
     }
+        int max = valids.get(0).length();
+        String longest = valids.get(0);
+        for (String valid : valids) {
+            int temp = valid.length();
+            if (temp > max) {
+                max = temp;
+                longest = valid;
+            }
+
+        }
+
+        return longest;
+
+}
+
 }
